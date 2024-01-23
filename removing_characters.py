d19c50bfee60d0b0177039c1f32d4fee028a2790
HannahGenerana <hannahgenerana@gmail.com>
2024-01-23 22:19:00 +0800

    pseudocode

---
+++ removing_characters.py
@@ -0,0 +1,14 @@
+# Exercise 4: Remove first n characters from a string
+# Write a program to remove characters from a string starting from zero up to n and return a new string.
+
+# pseudocode
+
+# create a function for the words and length of the n to be remove
+
+# print the word before removing characters
+
+# create a variable that will remove n characters from the word
+
+# return the new word
+
+# print the word with remove characters
